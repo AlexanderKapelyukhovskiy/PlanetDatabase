a9bd5b702e6b442a31d9c617c9877c3c71ab8e99
Alexander Kapelyukhovskiy <alkapa2005@gmail.com>
2015-05-04 01:43:34 +0300

    1) added HomeController
    2) removed Index.html

---
+++ PlanetDatabase.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace PlanetDatabase.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
